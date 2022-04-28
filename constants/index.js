58617999fb4f704592aa98800346b72390a9ae8a
Miguel Sarenas <migui0323@gmail.com>
2022-04-27 23:59:32 -0400

    new contract

--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,5 @@
 const WHITELIST_CONTRACT_ADDRESS = "0x30B9778A5a9FF5Bf4f786cB489EcE373a4235D6D";
 
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-kohl-five.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
